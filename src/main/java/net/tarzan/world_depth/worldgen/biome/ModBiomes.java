c52c37f2654aaef5875e503c38fc9c52e2d338eb
royaltarzan <royal.tarzan.speer@gmail.com>
2025-04-01 12:04:57 +0200

    Started work on custom biome

---
+++ src/main/java/net/tarzan/world_depth/worldgen/biome/ModBiomes.java
@@ -0,0 +1,46 @@
+package net.tarzan.world_depth.worldgen.biome;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.tarzan.world_depth.World_Depth;
+
+public class ModBiomes {
+    public static final ResourceKey<Biome> DEEP_PLAINS= ResourceKey.create(Registries.BIOME,
+            new ResourceLocation(World_Depth.MODID,"deep_plains"));
+
+    public static void bootstrap(BootstapContext<Biome> context){
+        context.register(DEEP_PLAINS, deep_plains(context));
+    }
+
+    private static Biome deep_plains(BootstapContext<Biome> context) {
+        MobSpawnSettings.Builder spawnBuilder=new MobSpawnSettings.Builder();
+
+        BiomeDefaultFeatures.farmAnimals(spawnBuilder);
+        BiomeDefaultFeatures.commonSpawns(spawnBuilder);
+
+        BiomeGenerationSettings.Builder biomeBuilder =
+                new BiomeGenerationSettings.Builder(context.lookup(Registries.PLACED_FEATURE), context.lookup(Registries.CONFIGURED_CARVER));
+
+        return new Biome.BiomeBuilder()
+                .hasPrecipitation(true)
+                .downfall(0.8f)
+                .temperature(0.7f)
+                .generationSettings(biomeBuilder.build())
+                .mobSpawnSettings(spawnBuilder.build())
+                .specialEffects((new BiomeSpecialEffects.Builder())
+                        .waterColor(0xe82e3b)
+                        .waterFogColor(0xbf1b26)
+                        .skyColor(0x30c918)
+                        .grassColorOverride(0x7f03fc)
+                        .foliageColorOverride(0xd203fc)
+                        .fogColor(0x22a1e6).build())
+                .build();
+    }
+}
---
+++ src/main/java/net/tarzan/world_depth/worldgen/biome/ModOverworldRegion.java
@@ -0,0 +1,4 @@
+package net.tarzan.world_depth.worldgen.biome;
+
+public class ModOverworldRegion {
+}
